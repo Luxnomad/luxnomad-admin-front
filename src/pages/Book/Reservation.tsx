0ab330df439b06ed39055e67d48c38865003431b
wongil12 <judgevi52@gmail.com>
2025-06-15 01:43:00 +0900

    feat: add reservation

---
+++ src/pages/Book/Reservation.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import PageTemplate from '@@components/PageTemplate';
+
+const StyledReservation = styled(PageTemplate)``;
+
+function Reservation() {
+  return <StyledReservation></StyledReservation>;
+}
+
+export default Reservation;
--- src/pages/Book/route.tsx
+++ src/pages/Book/route.tsx
@@ -1,12 +1,18 @@
 import { PATH } from '@@constants/path';
 import { USER_PERMISSION } from '@@constants/permissions';
 import Book from '@@pages/Book';
+import Reservation from '@@pages/Book/Reservation';
 import { RouteProps } from '@@router/types';
 
 export const bookRoute: RouteProps[] = [
   {
     path: PATH.BOOK,
     element: <Book />,
+    permission: USER_PERMISSION.READ,
+  },
+  {
+    path: `${PATH.BOOK}/reservation`,
+    element: <Reservation />,
     permission: USER_PERMISSION.READ,
   },
 ];
