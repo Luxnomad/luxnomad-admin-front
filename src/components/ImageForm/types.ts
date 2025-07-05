999a4e389c2a3578714c21c6e57485f03464fc65
wongil12 <judgevi52@gmail.com>
2025-07-05 22:15:21 +0900

    fix: remove image form

--- src/components/ImageForm/types.ts
+++
@@ -1,4 +0,0 @@
-import { UpsertPopupDTO } from '@@stores/popupManagement/types';
-import { UpsertPostDTO } from '@@stores/postManagement/types';
-
-export type FormDataType = UpsertPostDTO | UpsertPopupDTO;
