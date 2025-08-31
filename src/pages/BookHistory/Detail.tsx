766f38e832aa41e5d495fe73cf51f71d1f474c43
김원길 <judgevi52@gmail.com>
2025-08-31 16:26:09 +0900

    feat: add booking detail component

---
+++ src/pages/BookHistory/Detail.tsx
@@ -0,0 +1,7 @@
+import PageTemplate from '@@components/PageTemplate';
+
+function BookHistoryDetail() {
+  return <PageTemplate headerContent='Book Detail'></PageTemplate>;
+}
+
+export default BookHistoryDetail;
--- src/pages/BookHistory/route.tsx
+++ src/pages/BookHistory/route.tsx
@@ -3,10 +3,17 @@ import { USER_PERMISSION } from '@@constants/permissions';
 import BookHistory from '@@pages/BookHistory';
 import { RouteProps } from '@@router/types';
 
+import BookHistoryDetail from './Detail';
+
 export const bookHistoryRoute: RouteProps[] = [
   {
     path: PATH.BOOK_HISTORY,
     element: <BookHistory />,
+    permission: USER_PERMISSION.READ,
+  },
+  {
+    path: `${PATH.BOOK_HISTORY}/:id`,
+    element: <BookHistoryDetail />,
     permission: USER_PERMISSION.READ,
   },
 ];
