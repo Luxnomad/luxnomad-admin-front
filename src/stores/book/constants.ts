f29f24349f08739495b3b0fea3bdb2c7171a7e70
김원길 <judgevi52@gmail.com>
2025-08-31 16:17:58 +0900

    feat: change master card string

--- src/stores/book/constants.ts
+++ src/stores/book/constants.ts
@@ -1,7 +1,7 @@
 export const CREDIT_CARD_STRING: Record<string, string> = {
   VI: 'Visa',
   AX: 'American Express',
-  CA: 'MasterCard',
+  CA: 'Master Card',
   DC: 'Diners Club',
   JC: 'JCB',
   IK: 'Dankort',
