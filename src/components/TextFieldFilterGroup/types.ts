bb8da1c85d7aa287639c66da4a614da93b81edcd
김원길 <judgevi52@gmail.com>
2025-10-08 17:41:09 +0900

    fix: remove unused component

--- src/components/TextFieldFilterGroup/types.ts
+++
@@ -1,4 +0,0 @@
-import { FilterGroupProps } from '@@components/FilterGroup/types';
-import { TextFieldProps } from '@@components/TextField/types';
-
-export interface TextFieldFilterGroupProps extends TextFieldProps, Omit<FilterGroupProps, 'children'> {}
